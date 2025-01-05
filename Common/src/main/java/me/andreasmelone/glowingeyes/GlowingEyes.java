280bcd4522c017fe32793b5ab4a34e85426be4af
Andreas <andreykelbler@outlook.de>
2025-01-05 11:13:32 +0100

    Change default color to 242, 0, 0

--- Common/src/main/java/me/andreasmelone/glowingeyes/GlowingEyes.java
+++ Common/src/main/java/me/andreasmelone/glowingeyes/GlowingEyes.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class GlowingEyes {
 	public static final String MOD_ID = "glowingeyes";
 	public static final String MOD_NAME = "Glowing Eyes";
-	public static final Color DEFAULT_COLOR = new Color(255, 10, 10, 210);
+	public static final Color DEFAULT_COLOR = new Color(242, 0, 0, 210);
 	public static final Scheduler SCHEDULER_SERVER = new CodeScheduler();
 	public static final Scheduler SCHEDULER_CLIENT = new CodeScheduler();
 }
