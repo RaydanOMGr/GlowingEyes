1a7c2192c1cfa6a11d7dc8e74d2d8ef3fd143e3b
RaydanOMGr <andreykelbler@outlook.de>
2025-03-08 15:59:19 +0100

    Fixed a bug and updated dependencies

--- fabric/src/main/java/me/andreasmelone/glowingeyes/fabric/common/packet/HasModPacket.java
+++ fabric/src/main/java/me/andreasmelone/glowingeyes/fabric/common/packet/HasModPacket.java
@@ -8,6 +8,8 @@ import me.andreasmelone.glowingeyes.common.util.Util;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.PacketFlow;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 
 public class HasModPacket {
     public static final ResourceLocation ID = Util.id(GlowingEyes.MOD_ID, "has_mod");
@@ -22,8 +24,17 @@ public class HasModPacket {
     public void handle(PacketContext ctx) {
         ctx.enqueueWork(() -> {
             if (ctx.getDirection() == PacketFlow.SERVERBOUND) {
-                PlayerDataComponent.setHasMod(ctx.getSender(), true);
-                GlowingEyesComponent.sendUpdate(ctx.getSender());
+                ServerPlayer player = ctx.getSender();
+                PlayerDataComponent.setHasMod(player, true);
+                GlowingEyesComponent.sendUpdate(player);
+
+                for (Player trackedByPlayer : PlayerDataComponent.getTrackedBy(player)) {
+                    ServerPlayer trackedBy = (ServerPlayer) trackedByPlayer;
+                    if(PlayerDataComponent.hasMod(trackedBy)) {
+                        GlowingEyesComponent.sendUpdate(trackedBy, player);
+                    }
+                    GlowingEyesComponent.sendUpdate(player, trackedBy);
+                }
             }
         });
     }
